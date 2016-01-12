bb303892a953a93764dd2997aa286d1a3c6c38a7
kaizhengshu <air@zk.local>
2016-01-12 20:46:51 +0800

    First Commit

---
+++ HeMaLe/商城部分/StoreSectionModel.h
@@ -0,0 +1,13 @@
+//
+//  StoreSectionModel.h
+//  HeMaLe
+//
+//  Created by Air on 16/1/12.
+//  Copyright © 2016年 Air. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StoreSectionModel : NSObject
+
+@end
---
+++ HeMaLe/视频部分/VideoModel.m
@@ -0,0 +1,13 @@
+//
+//  VideoModel.m
+//  HeMaLe
+//
+//  Created by Air on 16/1/12.
+//  Copyright © 2016年 Air. All rights reserved.
+//
+
+#import "VideoModel.h"
+
+@implementation VideoModel
+
+@end
